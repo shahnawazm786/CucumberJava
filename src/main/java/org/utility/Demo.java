c8e2b8a4d1d8701ebcc42cefc46aafde1a1f3e1f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-07-27 12:00:06 +0530

    changes in the Demo.java file

--- src/main/java/org/utility/Demo.java
+++ src/main/java/org/utility/Demo.java
@@ -3,5 +3,6 @@ package org.utility;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(ReadPropertyFile.getProperty("browsername"));
+        System.out.println("Change of commit");
     }
 }
