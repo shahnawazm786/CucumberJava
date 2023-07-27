a3c6c005854725d4efe178f029806b45300e4fae
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-07-27 12:49:24 +0530

    Demo is saved in new-feature

--- src/main/java/org/utility/Demo.java
+++ src/main/java/org/utility/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println(ReadPropertyFile.getProperty("browsername"));
         System.out.println("Change of commit");
+        System.out.println("Change of commit");
     }
 }
