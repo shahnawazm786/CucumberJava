79689dc43b6e45e0fa7e8ca8f47651de0aa5b31c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-07-27 17:08:22 +0530

    feature file and step files

---
+++ src/test/java/awsome/HomeStepDefs.java
@@ -0,0 +1,16 @@
+package awsome;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+
+public class HomeStepDefs {
+    @Given("open the application")
+    public void openTheApplication(){
+
+    }
+    @And("verify the link of home page")
+    public void verifyTheLinkOfHomePage(){
+        
+    }
+
+}
--- src/test/resources/awesome/home_page.feature
+++ src/test/resources/awesome/home_page.feature
@@ -1,4 +1,7 @@
 @homepage
 Feature: Home  Page
+  @homepage_link
   Scenario: Verify the home page link
     Given open the application
+    And verify the link of home page
+
