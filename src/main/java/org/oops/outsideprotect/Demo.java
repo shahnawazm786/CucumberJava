25709124690e8ff3587def39eca9fc05b96d3c7f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-17 15:59:20 +0530

    protected Classes

---
+++ src/main/java/org/oops/outsideprotect/Demo.java
@@ -0,0 +1,11 @@
+package org.oops.outsideprotect;
+
+import org.oops.protect.ProtectExample;
+
+public class Demo extends ProtectExample {
+    public static void main(String[] args) {
+        Demo d=new Demo();
+        d.name="Abc";
+
+    }
+}
