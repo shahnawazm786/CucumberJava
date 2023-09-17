50c407d3102a9049c82758f19dce987262f08ff2
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-17 15:59:14 +0530

    protected Classes

---
+++ src/main/java/org/oops/defaultoutside/Demo.java
@@ -0,0 +1,10 @@
+package org.oops.defaultoutside;
+
+import org.oops.defaults.DefaultExample;
+
+public class Demo extends DefaultExample {
+    public static void main(String[] args) {
+        Demo d=new Demo();
+
+    }
+}
