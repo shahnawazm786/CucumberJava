6d982c05641ab1ab160881f46857bcbb3c8ec1ec
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-17 15:36:13 +0530

    protected Classes

--- src/main/java/outsiseprotect/OutsideClass.java
+++ src/main/java/outsiseprotect/OutsideClass.java
@@ -1,7 +1,11 @@
 package outsiseprotect;
 
+import protect.ProtectedExample;
+
 public class OutsideClass {
     public static void main(String[] args) {
-
+        ProtectedExample pe=new ProtectedExample();
+        //pe.name; // name is protected member in the class , outside the package it is not accessible
+        // so here name is not accessible
     }
 }
