f3185240b7d15283b3d4673fc750830d96b41f95
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-17 15:33:40 +0530

    protected Classes

--- src/main/java/protect/ProtectedDemo.java
+++ src/main/java/protect/ProtectedDemo.java
@@ -2,6 +2,7 @@ package protect;
 
 public class ProtectedDemo {
     public static void main(String[] args) {
-
+    ProtectedExample pe=new ProtectedExample();
+    pe.name="KAZ"; // within the package protected is accessible
     }
 }
