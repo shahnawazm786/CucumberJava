dd01747e88c253576393c9df3c8805adb20cf18b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-01 20:46:30 +0530

    Inheritance

---
+++ src/main/java/org/oops/inheritance/Demo.java
@@ -0,0 +1,9 @@
+package org.oops.inheritance;
+
+public class Demo {
+    public static void main(String[] args) {
+        InheritSingleInheritance iH=new InheritSingleInheritance("Mohammad","New Delhi",1200);
+        System.out.println(iH.show());
+    }
+
+}
