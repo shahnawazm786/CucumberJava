bf4a0e8427a6eede859a20ff94e942b1d07904d0
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-01 20:09:01 +0530

    Inheritance

--- src/main/java/org/oops/inheritance/InheritSingleInheritance.java
+++ src/main/java/org/oops/inheritance/InheritSingleInheritance.java
@@ -3,8 +3,13 @@ package org.oops.inheritance;
 public class InheritSingleInheritance extends SingleInheritanceExample{
     int empno;
 
+    public InheritSingleInheritance(String name, String address,int empno) {
+        super(name, address);
+        this.empno=empno;
+    }
+
     @Override
     public String show() {
-        return super.show();
+        return "employee no\n"+this.empno+"\n"+super.show();
     }
 }
