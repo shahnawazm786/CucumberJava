007721f27f74353d3d87153ff004a27756234bb8
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-01 20:04:14 +0530

    Inheritance

---
+++ src/main/java/org/oops/inheritance/SingleInheritanceExample.java
@@ -0,0 +1,9 @@
+package org.oops.inheritance;
+
+public class SingleInheritanceExample {
+    String name;
+    String address;
+    public String show(){
+        return "Name ->"+this.name+"\n Address ->"+this.address;
+    }
+}
