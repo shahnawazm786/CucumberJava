be34a54f5dc5962f05c5169d469ffe656fe23f2c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 19:13:32 +0530

    Inheritance

---
+++ src/main/java/org/lambda/callable/SumOfNumber.java
@@ -0,0 +1,7 @@
+package org.lambda.callable;
+
+public class SumOfNumber {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/org/oops/callable/SumofNumberUsingCalllable.java
+++
@@ -1,7 +0,0 @@
-package org.oops.callable;
-
-public class SumofNumberUsingCalllable {
-    public static void main(String[] args) {
-
-    }
-}
