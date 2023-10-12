6fbc2249f932d79b73474aa52d8beb831eaa8f2f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-12 20:27:23 +0530

    Inheritance

--- src/main/java/org/lambda/callable/SumOfNumber.java
+++ src/main/java/org/lambda/callable/SumOfNumber.java
@@ -1,6 +1,10 @@
 package org.lambda.callable;
 
+import java.util.stream.IntStream;
+
 public class SumOfNumber {
+    static int[] array= IntStream.rangeClosed(0,5000).toArray();
+    static int total=IntStream.rangeClosed(0,5000).sum();
     public static void main(String[] args) {
 
     }
